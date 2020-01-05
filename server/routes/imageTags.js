9c87a32be5fbb796647cfb75968aada03ab0d884
KathyPuma <kathypuma96@gmail.com>
2020-01-05 13:56:21 -0500

    install pg-promise and changed the pgConnectionString url

--- server/routes/imageTags.js
+++ server/routes/imageTags.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const pgp = require('pg-promise')();
-const connectString = 'postgres://localhost:5432/holding_db';
+const connectString = 'postgres://localhost:5432/bunkrbase';
 const db = pgp(connectString);
 
 
