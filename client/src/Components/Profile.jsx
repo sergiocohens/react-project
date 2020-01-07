792aba343f5534c915c17d20967723559546a5b4
sergiocohens <sergiocohensalama@pursuit.org>
2020-01-06 22:09:53 -0500

    profile-pic-changed

--- client/src/Components/Profile.jsx
+++ client/src/Components/Profile.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 import axios from 'axios'
 
 class Profile extends React.Component {
@@ -7,15 +7,37 @@ class Profile extends React.Component {
    this.state = {
      loggedUser: 'serg@gmail.com',
      loggedId:'2',
-     imgUrl: ''
+     imgUrl: '',
+     imgFile: null
    }
  }
 
  async componentDidMount() {
-   let response = await axios.get(`http://localhost:3001/users/${this.state.loggedId}`)
+   let response = await axios.get(`http://localhost:3001/users/profilepic/${this.state.loggedId}`)
    this.setState({
      imgUrl: response.data.body[0].img_url
    })
+ }
+
+ handleFileInput = (event) => {
+   this.setState({
+     imgFile: event.target.files[0]
+   })
+ }
+
+ handleSubmit = async (e) => {
+   e.preventDefault();
+   const data = new FormData()
+   data.append('image', this.state.imgFile)
+   try {
+     const res = await axios.post('http://localhost:3001/upload', data)
+     axios.put(`http://localhost:3001/users/profilepic/${this.state.loggedId}`, {imgUrl: res.data.imageUrl})
+     this.setState({
+       imgUrl: res.data.imageUrl
+     })
+   } catch (err) {
+     console.error(err)
+   }
  }
 
  render(){
@@ -24,6 +46,10 @@ class Profile extends React.Component {
             <h1>Profile</h1>
             <img src={this.state.imgUrl} alt=''></img>
             <p>Welcome {this.state.loggedUser}!</p>
+            <form onSubmit={this.handleSubmit}>
+              <input type="file" onChange={this.handleFileInput} />
+              <input type="submit" value="Change Pic" />
+            </form>
         </div>
       );
  }
