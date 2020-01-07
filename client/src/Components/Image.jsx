097ff85f489d0ed7b657f2a5322d89f03dd1cc32
RougeLight <dantaeflowers@pursuit.org>
2020-01-06 20:44:45 -0500

    Image Component changes

--- client/src/Components/Image.jsx
+++ client/src/Components/Image.jsx
@@ -1,21 +1,51 @@
 import React from 'react';
+import axios from 'axios';
+const multer = require ('multer');
+
 
 
 class Image extends React.Component {
- render(){
-    return (
-        <div className="App">
-        
-            <p>Image Upload</p>
-            <form>
-                <input type="file"/>
-                <input type="submit" value="Upload"/>
+    state = {
+        imageUrl: "",
+        imageFile: null
+      }
+    
+      handleFileInput = (event) => {
+        console.log('file changed')
+        this.setState({
+          imageFile: event.target.files[0]
+        })
+      }
+    
+      handleSubmit = async (e) => {
+        e.preventDefault();
+    
+        const data = new FormData()
+        data.append('image', this.state.imageFile)
+    
+        try {
+          const res = await axios.post('http://localhost:3001/upload', data)
+          console.log(res.data)
+          this.setState({
+            imageUrl: res.data.imageUrl
+          })
+        } catch (err) {
+          console.error(err)
+        }
+      }
+    
+      render() {
+        return (
+          <div className="App">
+            <form onSubmit={this.handleSubmit}>
+              <input type="file" onChange={this.handleFileInput} />
+              <input type="submit" value="Upload" />
             </form>
-
-        </div>
-      );
- }
-}
+            <img src={this.state.imageUrl} alt='random' />
+          </div>
+        );
+      }
+    }
 
 
 export default Image;
