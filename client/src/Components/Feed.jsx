fe78046253ab7739850dafc0167009cbbb80578a
gsanchez13 <gisellesanchez@pursuit.org>
2020-01-11 17:24:35 -0500

    deleted old burger files and created new component

--- client/src/Components/Feed.jsx
+++ client/src/Components/Feed.jsx
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-// import Image from './Image';
+import BurgerMenu from './BurgerMenu.jsx';
 import '../App.css';
-import { ThemeProvider } from 'styled-components';
-import { GlobalStyles } from './Burger/global';
-import { theme } from './Burger/theme';
 
 class Feed extends Component {
-    state = {
-        hashtag: "",
-        urls: [],
+    constructor() {
+        super()
+        this.state = {
+            hashtag: "",
+            urls: [],
+        }
+        this.ref = React.createRef();
     }
     handleInputChange = (e) => {
         this.setState({
@@ -21,45 +22,37 @@ class Feed extends Component {
         const { hashtag } = this.state
         let url = `http://localhost:3001/tags/${hashtag}`
         try {
-            let imageByTag = await axios.get(url).then((res => {return res.data.imgArr}))
-            let urlArr = imageByTag.map(elem => {return elem.img_src});
+            let imageByTag = await axios.get(url).then((res => { return res.data.imgArr }))
+            let urlArr = imageByTag.map(elem => { return elem.img_src });
             console.log(imageByTag)
             this.setState({
                 urls: urlArr,
             })
         }
-        catch(err) {
+        catch (err) {
             console.log("Error, something went wrong. ", err)
         }
     }
-    render () {
-        const { urls , hashtag } = this.state;
-            let images = urls.map((elem) => {
-                return <img src = {elem} alt ={hashtag}></img>
-            })
+    render() {
+        const { urls, hashtag } = this.state;
+        let images = urls.map((elem) => {
+            return <img src={elem} alt={hashtag}></img>
+        })
         return (
-            <div className = "feed">
-                <div className = "header">
+            <div className="feed">
+                <div className="header">
                     <h1> BUNKR </h1>
                 </div>
-                <nav className = "navBar">
-                <ThemeProvider theme={theme}>
-      <>
-        <GlobalStyles />
-        <div>
-          <h1>Hello. This is burger menu tutorial</h1>
-          <img src="https://image.flaticon.com/icons/svg/2016/2016012.svg" alt="burger icon" />
-        </div>
-      </>
-    </ThemeProvider>
-                </nav>
                 <div className="container">
                     <form onSubmit={this.getImagesByTags}>
                         <input type="text" placeholder="Search hashtags" onChange={this.handleInputChange}></input>
                     </form>
-                    <div className = "images">
-                    {images}
+                    <div className="images">
+                        {images}
                     </div>
+                    <div className="nav">
+                    <BurgerMenu />
+                </div>
                 </div>
             </div>
         )
