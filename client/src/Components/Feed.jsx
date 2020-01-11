7b8f50c78e78b497204fec3e002439aec7ce8528
gsanchez13 <gisellesanchez@pursuit.org>
2020-01-11 10:22:14 -0500

    css updates and beginning burger files

--- client/src/Components/Feed.jsx
+++ client/src/Components/Feed.jsx
@@ -1,12 +1,15 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
-import Image from './Image';
+// import Image from './Image';
 import '../App.css';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyles } from './Burger/global';
+import { theme } from './Burger/theme';
 
 class Feed extends Component {
     state = {
         hashtag: "",
-        urls: []
+        urls: [],
     }
     handleInputChange = (e) => {
         this.setState({
@@ -20,8 +23,9 @@ class Feed extends Component {
         try {
             let imageByTag = await axios.get(url).then((res => {return res.data.imgArr}))
             let urlArr = imageByTag.map(elem => {return elem.img_src});
+            console.log(imageByTag)
             this.setState({
-                urls: urlArr
+                urls: urlArr,
             })
         }
         catch(err) {
@@ -29,15 +33,26 @@ class Feed extends Component {
         }
     }
     render () {
-        const { urls , hashtag} = this.state;
-        let images = urls.map((elem) => {
-            return <img src = {elem} alt ={hashtag}></img>
-        })
+        const { urls , hashtag } = this.state;
+            let images = urls.map((elem) => {
+                return <img src = {elem} alt ={hashtag}></img>
+            })
         return (
             <div className = "feed">
                 <div className = "header">
                     <h1> BUNKR </h1>
                 </div>
+                <nav className = "navBar">
+                <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyles />
+        <div>
+          <h1>Hello. This is burger menu tutorial</h1>
+          <img src="https://image.flaticon.com/icons/svg/2016/2016012.svg" alt="burger icon" />
+        </div>
+      </>
+    </ThemeProvider>
+                </nav>
                 <div className="container">
                     <form onSubmit={this.getImagesByTags}>
                         <input type="text" placeholder="Search hashtags" onChange={this.handleInputChange}></input>
