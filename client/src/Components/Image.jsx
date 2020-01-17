85ef72b08a26777f9a7140d4fd6d939096f8aba5
KathyPuma <kathypuma96@gmail.com>
2020-01-17 08:10:51 -0500

    added routers for imageTags.js and able to post hastags when submit photo

--- client/src/Components/Image.jsx
+++ client/src/Components/Image.jsx
@@ -3,90 +3,158 @@ import axios from 'axios';
 import { BrowserRouter, Link, Redirect, Route, Switch } from 'react-router-dom'
 
 
-
 class Image extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-        imageUrl: "",
-        imageFile: null, 
-        loggedUser: 1,
-        tagIds:[],
-        tagStr:"",
-        tagList:""
-      }
+      imageUrl: "",
+      imageFile: null,
+      loggedUser: 1,
+      tagStr: "",
+      tagList: "",
+      tagId: null,
+      imgId: null,
+      tagsId: [],
+      tagsName: [],
+
     }
+  }
 
-      addTag = async () => {
-        let {tagStr} = this.state;
-        let tagId =   await axios.get(`http://localhost:3001/tags/tag/${tagStr}`) 
-        // if (!tagId.data.tagId.id) {
-        //   axios.post(`http://localhost:3001/`)
-        //   // function to post tag in tags table and then store tag id (tagIds) and tagname (tagList) in state
-        // } else {
-        //   // function to store tag id (tagIds) and tagname (tagList) in state
-        // }
 
+  handleFileInput = async (event) => {
+    this.setState({
+      imageFile: event.target.files[0],
+    })
+
+    const allImages = await axios.get('http://localhost:3001/images/all')
+    const allImagesData = allImages.data.payload
+    this.setState({
+      imgId: allImagesData[allImagesData.length - 1].id + 1
+    })
+  }
+
+
+  inputKeyDown = async (event) => {
+    this.setState({
+      tagStr: event.target.value
+    })
+    let tagStr = event.target.value
+    if (event.key === 'Enter' && tagStr) {
+      let tagIdExist = await axios.get(`http://localhost:3001/tags/tag/${tagStr}`).then((res) => { return res.data.success });
+      if (this.state.tagsName.find(tag => tag === tagStr)) {
+        return;
       }
-
-      handleFileInput = (event) => {
+      if (!tagIdExist) {
+        await axios.post(`http://localhost:3001/tags/tag/${tagStr}`);
+        let postedTagId = await axios.get(`http://localhost:3001/tags/tag/${tagStr}`).then((res) => { return res.data.tagId.id });
         this.setState({
-          imageFile: event.target.files[0],
-        //   loaded: 0
+          tagId: postedTagId,
+          tagsName: [...this.state.tagsName, tagStr],
+          tagsId: [...this.state.tagsId, postedTagId]
+        })
+      } else {
+        let tagId = await axios.get(`http://localhost:3001/tags/tag/${tagStr}`)
+        let tagIdData = tagId.data.tagId.id
+
+        this.setState({
+          tagId: tagIdData,
+          tagsName: [...this.state.tagsName, tagStr],
+          tagsId: [...this.state.tagsId, tagIdData]
         })
       }
+      this.tagInput.value = null;
+    }
+  }
 
-      handleTagsInput = (event) => {    
-        this.setState ({
-          tagStr: event.target.value
-        })
-        }
+
+
+
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    const { tagsId, imgId } = this.state
+    const data = new FormData()
+    //for(var x = 0; x<this.state.imageFile.length; x++){
+    data.append('image', this.state.imageFile)
+
+
+    try {
+      const res = await axios.post('http://localhost:3001/upload', data)
+
+      const post = axios.put(`http://localhost:3001/images/post`, { img_src: res.data.imageUrl, users_id: this.state.loggedUser })
+      // post tags to the imagestags table with tag_id (from this.state.tagIds) and img_id (from response from updated images table)
+
+
+      this.setState({
+        imageUrl: res.data.imageUrl,
+        //imageFile: event.target.files,
+        loaded: 0,
+
+
+      })
+
     
+    } catch (err) {
+      console.log(err)
+    }
+    this.postTags(tagsId, imgId)
+    this.handleResetTags()
+  }
 
-      handleSubmit = async (event) => {
-        event.preventDefault();
-        const data = new FormData()
-       //for(var x = 0; x<this.state.imageFile.length; x++){
-        data.append('image', this.state.imageFile)
 
+  postTags = async (tagsId, imgId) => {
+    if (imgId !== null) {
+
+      for (let i = 0; i < tagsId.length; i++) {
         try {
-          const res = await axios.post('http://localhost:3001/upload', data)
-          axios.put(`http://localhost:3001/images/post`, {img_src: res.data.imageUrl, users_id: this.state.loggedUser})
-          // post tags to the imagestags table with tag_id (from this.state.tagIds) and img_id (from response from updated images table)
-          this.setState({
-            imageUrl: res.data.imageUrl,
-            //imageFile: event.target.files,
-            loaded: 0,
-          })
-        } catch (err) {
-          console.error(err)
+          const addTagsTOImage = await axios.post(`http://localhost:3001/imageTags/addTagToImage/`, { tag_id: tagsId[i], img_id: imgId })
+          const getallTagsAndIds = await axios.get(`http://localhost:3001/imageTags/`)
+        } catch (error) {
+          console.log(error)
         }
-      }
-
-      render() {
-
-        const { loggedUser, tagList} = this.state
-        
-        return (
-          <div className="App">
-            <form onSubmit={this.handleSubmit}>
-                <p>Upload image</p>
-                <input type="file" onChange={this.handleFileInput} placeholder="Write tags with hashtags" />                
-                <br></br>
-                <p>Add Tags</p><input type="text" onChange={this.handleTagsInput}/>
-                <button onClick={this.addTag}>Add</button> <p>Tags:{tagList}</p>
-                <input type="submit" value="Upload" />
-            </form>
-            {/* <form onSubmit={this.handleTags}>
-                <p>Tag:</p>
-                <input type="text"/>
-                <input type="submit"/>
-            </form> */}
-            <img src={this.state.imageUrl} alt="" className='images' />
-          </div>
-        );
       }
     }
+this.handleResetTags()
+  }
+
+
+  handleResetTags = () => {
+    this.setState({
+      tagsId: [],
+      tagsName: [],
+    })
+  }
+  
+
+
+
+
+
+  render() {
+    const { loggedUser, tagList, tagIds, tagStr, tagsName } = this.state
+    return (
+      <div className="App">
+        <form onSubmit={this.handleSubmit}>
+          <p>Upload image</p>
+          <input type="file"
+            onChange={this.handleFileInput} placeholder="Write tags with hashtags" />
+          <br></br>
+          <input type="submit" value="Upload" />
+
+
+        </form>
+        <ul className="input-tag__tags">
+
+          {tagsName.map((tagsName, i) => (
+            <li key={tagsName}>
+              {tagsName}
+            </li>
+          ))}
+          <li className="input-tag__tags__input"><input type="text" placeholder="InsertTags" onKeyDown={this.inputKeyDown} ref={c => { this.tagInput = c; }} /></li>
+        </ul>
+      </div>
+    );
+  }
+}
 
 
 export default Image;
--- server/routes/imageTags.js
+++ server/routes/imageTags.js
@@ -3,11 +3,43 @@ const router = express.Router();
 const db = require('./pgExport');
 
 
+router.get('/', async (req, res) => {
+    try {
+        let response = await db.any('SELECT * FROM image_tags;');
+       
+        res.json({
+            success: true,
+            body: response
+        })
+    }
+    catch(err) {
+        res.send({
+            success: false,
+            error: "Data does not exist"
+        })
+    }
+
+})
 
 
 
-
-
-
+router.post('/addTagToImage/', async (req,  res) => {
+    try {
+        let insertQuery = `INSERT INTO image_tags(tag_id, img_id)
+        VALUES ($1, $2);`
+        db.none(insertQuery, [req.body.tag_id, req.body.img_id])
+        res.json({
+            success: true,
+            body:req.body,
+            message: "tags and Image Inserted"
+        })
+    }
+    catch(err) {
+        res.send({
+            success: false,
+            error: "Cannot add tag with images"
+        })
+    }
+})
 
 module.exports = router;
